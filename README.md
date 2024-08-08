3bb6b7c81d9118f2cb54612f8c1d33adaf7dac3c
Micah Wylde <micah@arroyo.systems>
2024-08-08 11:37:54 -0700

    Add tutorial files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Arroyo Fly Tutorial
+
+This repository contains an example of how to run Arroyo on Fly.io. See the [blog post](https://www.arroyo.dev/blog/arroyo-on-fly) for
+full details on how to deploy this.
--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 import redis
 import json
+import os
+
 
 app = Flask(__name__)
 
 # Connect to Redis
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+r = redis.Redis(host='fly-arroyo-fly-redis.upstash.io', port=6379, password=os.environ["REDIS_PASSWORD"], db=0, decode_responses=True)
 
 def get_editors_data():
     editors_data = r.hgetall('top_editors')
@@ -24,4 +26,4 @@ def refresh():
     return render_template('table_content.html', editors=editors)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
